45d29b1bea822bb66258ca59ad34f42349552cad
Hexiang Qi <qihexiang@outlook.com>
2021-04-03 00:52:08 +0800

    Fix bugs with resBuilder.file

--- src/resBuilder/file.ts
+++ src/resBuilder/file.ts
@@ -4,15 +4,18 @@ import { getType } from "mime";
 import { responseBody } from "../usr/Basic";
 import { stream } from "./stream";
 import { httpError } from "./error";
+import { handleErr } from "../utils/handleError";
 
 export async function file(path: string): Promise<responseBody> {
     try {
-        await stat(path);
+        if (!(await stat(path)).isFile()) throw new Error("Not a generic file.");
         const rStream = createReadStream(path);
         const resBody = stream(rStream);
         resBody.header["Content-Type"] = getType(path) || 'application/octect-stream';
         return resBody;
     } catch (err) {
+        if (err instanceof Error) handleErr(err);
+        else handleErr(new Error(err));
         return httpError(404, "File not found");
     }
 }
