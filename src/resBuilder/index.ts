5050eef5d9fa6ad1d51f7353525ee26dc350f59a
Hexiang Qi <qihexiang@outlook.com>
2021-04-03 00:21:07 +0800

    Export resBuilder.httpError

--- src/resBuilder/index.ts
+++ src/resBuilder/index.ts
@@ -2,7 +2,8 @@ import { text, html, css, js } from "./text";
 import { json } from "./json";
 import { stream, buffer } from "./stream";
 import { file } from "./file";
+import { httpError } from "./error";
 export const resBuilder = {
-    text, html, css, js, json, stream, buffer, file
+    text, html, css, js, json, stream, buffer, file, httpError
 };
 export default resBuilder;
