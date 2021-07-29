bb203d12bef9ba00c74bce8a0d198f3ca0b2653f
Hexiang Qi <qihexiang@outlook.com>
2021-07-29 08:14:03 +0800

    Update matcher type definition

--- src/router/matcher.ts
+++ src/router/matcher.ts
@@ -1,9 +1,18 @@
-export function condition<T>(reality: string) {
+interface ConditionCallback<T> {
+    (condition: string): T;
+}
+
+interface Condition<T> {
+    match: (condition: string, callback: ConditionCallback<T>) => Condition<T>;
+    getResult: () => T | null;
+}
+
+export function condition<T>(reality: string): Condition<T> {
     let result: T | null = null;
     function getResult() {
-        return result
+        return result;
     }
-    function match(condition: string, callback: (condition: string) => T) {
+    function match(condition: string, callback: (condition: string) => T): Condition<T> {
         if (condition === reality) result = callback(reality);
         return {
             match, getResult
