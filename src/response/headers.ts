84508aaef1f2bcc5691c223ff87021c676a48eab
Hexiang Qi <qihexiang@outlook.com>
2021-07-30 21:18:14 +0800

    新增了一些头设置函数

--- src/response/headers.ts
+++ src/response/headers.ts
@@ -1,7 +1,30 @@
 import { getType } from "mime";
 
-export const contentType = (typename: string) => ({ "Content-Type": getType(typename) ?? "application/octect-stream" });
-export const contentLength = (length: number) => {
-    if (Number.isInteger(length)) return { "Content-Length": length };
-    return {};
-};
+export function contentType(mediaType: string, charset?: string) {
+    return { "Content-Type": `${getType(mediaType)}${charset ? `; charset=${charset.toUpperCase()}` : ""}` };
+}
+
+export function contentLength(length: number) {
+    const check = Number.isInteger(length);
+    if (!check) console.log(`Content-Length should be an integer`);
+    return check ? { "Content-Length": length } : {};
+}
+
+export function contentDisposition(download: false): { "Content-Disposition": string; };
+export function contentDisposition(download: true, filename?: string): { "Content-Disposition": string; };
+export function contentDisposition(download: boolean, filename?: string) {
+    return { "Content-Disposition": `${download ? "attachment" : "inline"}${filename ? `; filename=${filename}` : ""}` };
+}
+
+type EncodingType = "gzip" | "compress" | "deflate" | "identity" | "br";
+export function contentEncoding(encodingType: EncodingType) {
+    return { "Content-Encoding": encodingType };
+}
+
+export function contentLanguage(languageTag: string) {
+    return { "Content-Language": languageTag };
+}
+
+export function serverHeader(product: string = "anelsonia2") {
+    return { "Server": product };
+}
