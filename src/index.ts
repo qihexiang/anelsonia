f2c1bf56ca69665e95b1f5889bc527c16a2a058c
Hexiang Qi <qihexiang@outlook.com>
2022-01-19 22:34:13 +0800

    Add AsyncResponse to index export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { shimHTTP } from "./core/shimHTTP";
 export { shimKoa } from "./core/shimKoa";
-export { Respond, createRes } from "./core/Respond";
+export { Respond, createRes, AsyncResponse } from "./core/Respond";
 export { composeFn } from "./utils/composeFn";
 export { ResHeader } from "./utils/ResHeader";
 export { createWrapper } from "./utils/createWrapper";
