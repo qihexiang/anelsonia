b4e552898c1b2c9c5170cd17be98139665c70c36
祁鹤翔 <qihexiang@outlook.com>
2022-02-02 14:31:27 +0800

    Add comment docs forr shimHTTP

--- src/core/shimHTTP.ts
+++ src/core/shimHTTP.ts
@@ -10,9 +10,15 @@ export type HttpRes = ServerResponse | Http2ServerResponse;
 export type ReqHandler = (req: HttpReq, res: HttpRes) => void;
 export type EntryPoint = (req: HttpReq) => AsyncResponse;
 
-const reqSyms = new AsyncLocalStorage<HttpReq>();
+const requests = new AsyncLocalStorage<HttpReq>();
+
+/**
+ * Get `HttpReq` from anywhere called by callback of shimHTTP
+ * 
+ * @returns `HttpReq` object
+ */
 export const useRequest = () => {
-    const req = reqSyms.getStore();
+    const req = requests.getStore();
     if (req === undefined) throw new Error("Can't get request, is this function called by main function?");
     return req;
 };
@@ -23,12 +29,21 @@ interface CreateFlare {
     <T>(options: { mutable: false, reassign: boolean; }): [(value: T) => Readonly<void>, () => T, () => void];
 }
 
+/**
+ * Create a flare and return handle functions.
+ * 
+ * @param options `{ mutable: boolean, reassign: boolean }` define it this flare mutable and reassignable
+ * @returns `[light, observe, extinguish]` 
+ * - `light`  assign value to the flare
+ * - `observe` get the value from the flare
+ * - `extinguish` remove the value from the flare
+ */
 export const createFlare: CreateFlare = <T>(options = {
     mutable: false, reassign: false
 }) => {
     const { reassign } = options;
     const getReq = () => {
-        const result = reqSyms.getStore();
+        const result = requests.getStore();
         if (result === undefined) throw new Error("Can't get symbol of this request.");
         return result;
     };
@@ -49,9 +64,16 @@ export const createFlare: CreateFlare = <T>(options = {
     return [light, observe, extinguish];
 };
 
+/**
+ * Transform an entry function to Node.js HTTP request handler
+ * 
+ * @param entry a function receives request object and return a reponse object.
+ * @param errHandler handler for dealing with errors
+ * @returns a handler function for Node.js `http`、`https`、`http2` modules
+ */
 export function shimHTTP(entry: EntryPoint, errHandler?: (err: any) => void): ReqHandler {
     return async (req, res) => {
-        reqSyms.run(req, async () => {
+        requests.run(req, async () => {
             try {
                 const { statusCode, statusMessage, body, headers } = await entry(req);
                 if (res instanceof IncomingMessage) res.writeHead(statusCode, statusMessage, headers);
