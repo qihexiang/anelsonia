0b79312c785127795840763b4e9276f65963222d
Hexiang <qihexiang@outlook.com>
2022-02-15 10:44:19 +0800

    optimize isVoid return type

--- src/utils/isVoid.ts
+++ src/utils/isVoid.ts
@@ -4,13 +4,15 @@ type Voids<T extends Void> = T[];
 export const fullIsVoid = <V extends Void>(
     initValue: unknown,
     voidValues: Voids<V>
-): initValue is V => {
+): initValue is Extract<typeof initValue, V> => {
     return voidValues.reduce(
         (current, next) => current || next === initValue,
         false as boolean
     );
 };
 
-export const isVoid = (initValue: unknown): initValue is undefined | null => {
+export const isVoid = (
+    initValue: unknown
+): initValue is Extract<typeof initValue, undefined | null> => {
     return fullIsVoid(initValue, [undefined, null]);
 };
