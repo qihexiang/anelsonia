6e33f61ba9f2603fe82c4b26265fdecfbd89ad6b
Hexiang <qihexiang@outlook.com>
2022-02-15 11:12:43 +0800

    fix bugs that isVoid always return value is never

--- src/utils/isVoid.ts
+++ src/utils/isVoid.ts
@@ -1,18 +1,18 @@
 type Void = void | undefined | null | "" | 0;
 type Voids<T extends Void> = T[];
 
-export const fullIsVoid = <V extends Void>(
-    initValue: unknown,
+export const fullIsVoid = <T, V extends Void>(
+    initValue: T,
     voidValues: Voids<V>
-): initValue is Extract<typeof initValue, V> => {
+): initValue is Extract<T, V> => {
     return voidValues.reduce(
-        (current, next) => current || next === initValue,
+        (current, next) => current || (next as unknown) === initValue,
         false as boolean
     );
 };
 
-export const isVoid = (
-    initValue: unknown
-): initValue is Extract<typeof initValue, undefined | null> => {
+export const isVoid = <T>(
+    initValue: T
+): initValue is Extract<T, undefined | null> => {
     return fullIsVoid(initValue, [undefined, null]);
 };
