2488fa744e2a081bb83b9a788b881e9fdf83d82a
Hexiang Qi <qihexiang@outlook.com>
2022-02-23 08:59:40 +0800

    Add All and AllX function to support allow all methods.

--- src/router/methods.ts
+++ src/router/methods.ts
@@ -1,6 +1,14 @@
 import { useURL } from "../core/shimHTTP";
 import { RouteHandler, RouteHandlerX } from "./createRoute";
 
+/**
+ * Limit HTTP methods on a route.
+ * 
+ * @param handler the route handler.
+ * @param methods an array of methods, like `["GET", "POST"]`
+ * @returns a new handler, which will return null when
+ * methods is not allowed.
+ */
 export const allowMethods = <P extends string, R>(
     handler: RouteHandler<P, R>,
     methods: string[]
@@ -11,6 +19,8 @@ export const allowMethods = <P extends string, R>(
     };
 };
 
+export const All = <P extends string, R>(handler: RouteHandler<P, R>) =>
+    allowMethods(handler, ["GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"]);
 export const Get = <P extends string, R>(handler: RouteHandler<P, R>) =>
     allowMethods(handler, ["GET"]);
 export const Head = <P extends string, R>(handler: RouteHandler<P, R>) =>
@@ -30,6 +40,14 @@ export const Trace = <P extends string, R>(handler: RouteHandler<P, R>) =>
 export const Patch = <P extends string, R>(handler: RouteHandler<P, R>) =>
     allowMethods(handler, ["PATCH"]);
 
+/**
+ * Limit HTTP methods on a extended route.
+ * 
+ * @param handler the extended route handler.
+ * @param methods an array of methods, like `["GET", "POST"]`
+ * @returns a new extended handler, which will return null when
+ * methods is not allowed.
+ */
 export const allowMethodsX = <P extends string, X, R>(
     handler: RouteHandlerX<P, X, R>,
     methods: string[]
@@ -40,6 +58,9 @@ export const allowMethodsX = <P extends string, X, R>(
     };
 };
 
+
+export const AllX = <P extends string, X, R>(handler: RouteHandlerX<P, X, R>) =>
+    allowMethodsX(handler, ["GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"]);
 export const GetX = <P extends string, X, R>(handler: RouteHandlerX<P, X, R>) =>
     allowMethodsX(handler, ["GET"]);
 export const HeadX = <P extends string, X, R>(
