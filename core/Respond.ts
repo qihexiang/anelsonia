584900e0027f69969da12601e8a692f5c32917e2
Hexiang <qihexiang@outlook.com>
2022-03-02 21:03:36 +0800

    Finish all comment docs in core module

--- core/Respond.ts
+++ core/Respond.ts
@@ -1,7 +1,18 @@
 import { isVoid } from "../utils/isVoid.ts";
 
+type Body = BodyInit;
+
+/**
+ * ResponseBody is a subset of Deno BodyInit, which includes 
+ * `string`, `Blob` and `ReadableStream` of `Uint8Array`
+ */
 export type ResponseBody = string | Blob | ReadableStream<Uint8Array>;
 
+/**
+ * ResponseProps is the return type of a EntryPoint function, which includes
+ * everything that a response need: status code, status meassage, response body 
+ * and response headers.
+ */
 export interface ResponseProps {
     readonly status: number;
     readonly statusText?: string;
@@ -9,24 +20,116 @@ export interface ResponseProps {
     readonly headers: Record<string, string>;
 }
 
-export interface Respond extends ResponseProps {
+/**
+ * Respond is extended from a readonly ResponseProps, which 
+ * means that you can't modify the properties of the object.  
+ * Respond provides 4 methods to create a new Respond object 
+ * from the old one.
+ */
+export interface Respond extends Readonly<ResponseProps> {
+    /**
+     * Generate a new Repond object with a new status code
+     * 
+     * @param code status code, recommended to set is with 
+     * `Status` enum from http_status.ts in standard library 
+     */
     setStatus(code: number): Respond;
+    /**
+     * Generate a new Respond object with a new status message.
+     * 
+     * @param text status message, this may not work with HTTP/2 
+     * and some clients.
+     */
     setStatusText(text: string): Respond;
+    /**
+     * Generate a new Respond object with a new response body.
+     * 
+     * @param body response body, if your status is 204, just 
+     * left it undefined.
+     */
     setBody(body: ResponseBody): Respond;
+    /**
+     * Generate a new Respond object with a new response header, 
+     * by given header name an the value.
+     * 
+     * @param headerName the header name.
+     * @param headerValue the value of header.
+     */
     setHeaders(headerName: string, headerValue: string): Respond;
+    /**
+     * Generate a new Respond object with one or more response
+     * headers.
+     * 
+     * @param headers headers in array or object format, like:
+     * 
+     * ```ts
+     * const res = createRes()
+     *     .setStatus(200)
+     *     .setBody("hello, world")
+     *     // in array format
+     *     .setHeaders(["Content-Type", "text/plain; charset=UTF-8"], ["Content-Length", "12"])
+     *     // in object format
+     *     .setHeaders({
+     *         "Connection": "keep-alive", "Keep-Alive": "timeout=5"
+     *     })
+     *     // mixed style is also ok
+     *     .setHeaders({"Server": "Freesia"}, ["Date", new Date().toLocaleString()])
+     * ```
+     */
     setHeaders(...headers: Headers[]): Respond;
 }
 
+/**
+ * Define a header in an tuple [key, value].
+ */
 type ArrayHeaders = [string, string];
+/**
+ * Define headers in an object.
+ */
 type RecordHeaders = Record<string, string>;
+/**
+ * Union type of ArrayHeaders and RecordHeaders.
+ */
 type Headers = ArrayHeaders | RecordHeaders;
 
+/**
+ * Create an empty Respond with status code 404.
+ */
 export function createRes(): Respond;
+/**
+ * Create an empty Respond with give status code.
+ * 
+ * @param code 
+ */
 export function createRes(code: number): Respond;
+/**
+ * Create a Respond with a body, default status code is 200.
+ * 
+ * @param body the response body
+ */
 export function createRes(body: ResponseBody): Respond;
+/**
+ * Create a Respond and set both code and body
+ * 
+ * @param code the status code 
+ * @param body the response body
+ */
 export function createRes(code: number, body: ResponseBody): Respond;
+/**
+ * Create a Respond and set both code and headers, and left body empty
+ * 
+ * @param code the status code
+ * @param headers a header in array or object format
+ */
 export function createRes(code: number, headers: Headers): Respond;
 export function createRes(body: ResponseBody, headers: Headers): Respond;
+/**
+ * Create a Respond with given status code, response body and response headers.
+ * 
+ * @param code the status code
+ * @param body the response body
+ * @param headers a header in array or object format
+ */
 export function createRes(
     code: number,
     body: ResponseBody,
--- core/shimHTTP.ts
+++ core/shimHTTP.ts
@@ -1,11 +1,21 @@
 import { ResponseProps } from "./Respond.ts";
 import { MaybePromise } from "../utils/MaybePromise.ts";
 
-export type HttpReq = Request;
-export type HttpRes = Response;
+/**
+ * ReqHandler is the type of handler for serve function in http module of Deno standard library
+ */
 export type ReqHandler = (req: Request) => MaybePromise<Response>;
-export type EntryPoint = (req: HttpReq) => Promise<ResponseProps>;
+/**
+ * EntryPoint is the type of main function of a Freesia application
+ */
+export type EntryPoint = (req: Request) => MaybePromise<ResponseProps>;
 
+/**
+ * Transform an EntryPoint function to a handler for serve function
+ *
+ * @param main the main function of your application.
+ * @returns a handler for serve function
+ */
 export const shimHTTP = (main: EntryPoint) => {
     return async (req: Request) => {
         const { status, statusText, body, headers } = await main(req);
