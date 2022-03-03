28cccb6a6da0ac9115b21d5d151d0f95cd477635
Hexiang <qihexiang@outlook.com>
2022-03-03 09:20:49 +0800

    Fix bugs in comment doc

--- router/params.ts
+++ router/params.ts
@@ -1,7 +1,7 @@
 export type Empty = Record<never, never>;
 /**
- * Pattern <T> matches with symbol +, which means there must be at least one character,
- * Pattern <[T]> matches with symbol + like pattern <T>, but it's greedy.
+ * Pattern `<T>` matches with symbol +, which means there must be at least one character,
+ * Pattern <[T]> matches with symbol + like pattern `<T>`, but it's greedy.
  * Pattern [T] matches with symbol *, which means there can be 0 character, it's greedy too.
  */
 export type ParamFlag<T extends string> = `[${T}]` | `<[${T}]>` | `<${T}>`;
@@ -10,6 +10,6 @@ export type RouteParam<U extends string> = U extends RoutePattern<
     infer L,
     infer R
 > ? L extends ParamFlag<infer T> ? Record<T | keyof RouteParam<R>, string>
-: Record<keyof RouteParam<R>, string>
+    : Record<keyof RouteParam<R>, string>
     : U extends ParamFlag<infer T> ? Record<T, string>
     : Empty;
